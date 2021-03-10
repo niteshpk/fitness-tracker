b8cf0937a29a2eba85e2d2ed0745b9734829deea
Nitesh Kesarkar <niteshkesarkar136@gmail.com>
2021-03-10 12:17:41 +0530

    Updated angular to latest version and pagination, sorting and filtering issue fixed

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -5,10 +5,10 @@ import { Exercise } from './excercise.model';
 @Injectable()
 export class TrainingService {
   private availableExercises: Exercise[] = [
-    { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
-    { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
-    { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
-    { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 },
+    { id: 'crunches', name: 'Crunches', duration: 1, calories: 8 },
+    { id: 'touch-toes', name: 'Touch Toes', duration: 2, calories: 15 },
+    { id: 'side-lunges', name: 'Side Lunges', duration: 3, calories: 18 },
+    { id: 'burpees', name: 'Burpees', duration: 4, calories: 4 },
   ];
   private runningExercise: Exercise;
   public exerciseChanged = new Subject<Exercise>();
